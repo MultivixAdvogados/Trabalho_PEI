10cc4333d319df1aedfbf62ae9b4d03ae0249290
Gabriel <60259949+agabundo@users.noreply.github.com>
2024-05-23 15:43:20 -0300

    Update Script.js

---
+++ Script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = document.querySelector('.navigation .menu');
+    const navMenu = document.querySelector('.navigation .nav-menu');
+    menu.classList.toggle('open');
+    navMenu.classList.toggle('open');
+}
