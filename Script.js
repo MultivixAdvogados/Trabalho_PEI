fd4eeaaccd6fb3f4eacd0682cb6ac6a313564b98
Gabriel <60259949+agabundo@users.noreply.github.com>
2024-05-28 12:59:34 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -4,12 +4,7 @@ function toggleMenu() {
     menu.classList.toggle('open');
     navMenu.classList.toggle('open');
 }
-function updateZoom() {
-    var zoomPercentage = document.getElementById('zoomPercentage').value / 100;
-    renderPDF(zoomPercentage);
-}
-
-function renderPDF(zoom) {
+document.addEventListener("DOMContentLoaded", function() {
     var pdfElement = document.getElementById('pdf-viewer');
     var pdfUrl = 'pdf/PEI_Franc.pdf';
 
@@ -18,7 +13,7 @@ function renderPDF(zoom) {
         pdf.getPage(1).then(function(page) {
             var canvas = document.createElement('canvas');
             var context = canvas.getContext('2d');
-            var viewport = page.getViewport({ scale: zoom });
+            var viewport = page.getViewport({ scale: 1.5 });
             canvas.height = viewport.height;
             canvas.width = viewport.width;
 
@@ -28,13 +23,7 @@ function renderPDF(zoom) {
             };
 
             page.render(renderContext);
-            pdfElement.innerHTML = ''; // Limpar o conteúdo anterior
             pdfElement.appendChild(canvas);
         });
     });
-}
-
-// Renderizar PDF com zoom padrão ao carregar a página
-document.addEventListener("DOMContentLoaded", function() {
-    updateZoom();
 });
